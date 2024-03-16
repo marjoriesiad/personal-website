168aed6ec4ef1e5faf0877f300da04f295f846c8
Marjorie Siad <marjoriesiad@gmail.com>
2024-03-16 17:09:38 +0100

    add homepage with non-functionnal pagination

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="">
+<div class="h-full flex justify-center items-center">
   <div class="bg-windowsgray w-5/6 h-auto border-2 border-r-gray-500 border-b-gray-500  md:max-w-[60%] md:min-h-[80%] absolute">
     <div class="flex justify-between bg-gradient-to-r from-windowsblue to-linearblue px-3 h-6 items-center">
             <p class="text-white">Welcome</p>
@@ -7,8 +7,21 @@
                 <p class="font-bold">x</p>
             </div>
         </div>
-        test test
-        test
+        <!-- CONTENT -->
+        <div class="flex justify-center pt-2">
+          <img src="../assets/images/avatar.png" alt="" class="w-32 rounded-full border-2 border-gray-300 shadow-lg">
+        </div>
+        <div class="p-2 text-md">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque quisquam neque quis commodi! Accusamus nemo dicta in, neque aliquam voluptatem sit rerum ullam. Non a quidem culpa ratione nobis fugiat?
+        </div>
+        <div class="flex justify-center items-center pb-2 space-x-3">
+          <a href="#" class="border-2 border-r-gray-500 border-b-gray-500 w-5 h-5 flex justify-center items-center"><</a>
+          <a href="#">1</a>
+          <a href="#">2</a>
+          <a href="#">3</a>
+          <a href="#" class="border-2 border-r-gray-500 border-b-gray-500 w-5 h-5 flex justify-center items-center">></a>
+        </div>
+       
         </div>
 </div>
 </template>
