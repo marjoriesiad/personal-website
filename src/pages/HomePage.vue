621679ee87677fd80c03997ce8268c4977f1c34f
Marjorie Siad <marjoriesiad@gmail.com>
2024-03-16 17:23:50 +0100

    fixed home page for desktop

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="h-full flex justify-center items-center">
-  <div class="bg-windowsgray w-5/6 h-auto border-2 border-r-gray-500 border-b-gray-500  md:max-w-[60%] md:min-h-[80%] absolute">
+  <div class="bg-windowsgray max-w-[70%] max-h-[80%] absolute border-2 border-r-gray-500 border-b-gray-500 md:max-w-[50%]">
     <div class="flex justify-between bg-gradient-to-r from-windowsblue to-linearblue px-3 h-6 items-center">
             <p class="text-white">Welcome</p>
             <div class="bg-windowsgray flex items-center justify-center w-4 h-4 border border-r-gray-500 border-b-gray-500">
@@ -8,13 +8,13 @@
             </div>
         </div>
         <!-- CONTENT -->
-        <div class="flex justify-center pt-2">
-          <img src="../assets/images/avatar.png" alt="" class="w-32 rounded-full border-2 border-gray-300 shadow-lg">
+        <div class="flex justify-center pt-3 md:p-5">
+          <img src="../assets/images/avatar.png" alt="" class="w-32 rounded-full border-2 border-gray-300 shadow-lg md:w-52">
         </div>
-        <div class="p-2 text-md">
+        <div class="text-md p-3 md:px-20 md:text-lg">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque quisquam neque quis commodi! Accusamus nemo dicta in, neque aliquam voluptatem sit rerum ullam. Non a quidem culpa ratione nobis fugiat?
         </div>
-        <div class="flex justify-center items-center pb-2 space-x-3">
+        <div class="flex justify-center items-center pb-4 space-x-3 relative bottom-0">
           <a href="#" class="border-2 border-r-gray-500 border-b-gray-500 w-5 h-5 flex justify-center items-center"><</a>
           <a href="#">1</a>
           <a href="#">2</a>
