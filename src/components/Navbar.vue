25356999297f0f7e18573a01a9fbc23d49961608
Marjorie Siad <marjoriesiad@gmail.com>
2024-03-16 12:42:43 +0100

    creating base navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+    <div class="bg-windowsgray flex justify-between items-center py-7 h-10 w-screen fixed bottom-0">
+        <div class="flex justify-center items-center m-2 space-x-2 cursor-pointer border-2 p-2 border-r-gray-500 border-b-gray-500">
+            <img src="../assets/images/start.png" alt="" class="w-24">
+        </div>
+            
+        <div class="p-2 px-3 mr-2 border-2 border-t-gray-500 border-r-white border-b-white border-l-gray-500 font-bold">
+            12:41
+        </div>
+    </div>    
+</template>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,3 +1,3 @@
 <template>
-    <h1 class="text-red-500 font-bold underline">Test</h1>
+
 </template>
